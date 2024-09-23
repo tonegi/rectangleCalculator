36c45c09a4a890a743061794ddac9642226900a8
tonegi <90652377+tonegi@users.noreply.github.com>
2024-09-23 17:02:15 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 
 int main(void) {
     int base, height, area;
-  printf("Type the base of the rectangle in centimeters: ");
+  printf("Enter the base of the rectangle in centimeters: ");
       scanf("%d",&base);
-  printf("Type the height of the rectangle in centimeters: ");
+  printf("Enter the height of the rectangle in centimeters: ");
       scanf("%d",&height);
     area = base*height;
   printf("The area of the rectangle is: %dcm", area);    
